ab49610422fef35a960447d8e4dca50dadc02b9a
troycalliste <troycallist@gmail.com>
2019-08-16 18:27:45 -0400

    call to speech api

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,11 +14,22 @@ class StaticPagesController < ApplicationController
     @ourUrl = params[:data_value]
     doc = Nokogiri::HTML(RestClient.get(@ourUrl))
     arr = []
+    frarr = []
+    harr = []
     entries = doc.css('#examples-content')
-    @entries = entries.css('.src')
-    @entries.to_a.take(10).each_slice(2) do |a, b|
-     arr << a.css('span').css('em').text
-     # arr << b.css('span').text
+    @entries = entries.css('.src').css('.text')
+    @frentries = entries.css('.trg')
+    @entries.to_a.take(10).each do |a|
+     arr << a.text
+
+
+     # arr << a.css('span').css('em').text
+   end
+   @frentries.take(20).each_slice(2) do |f, b|
+     frarr << f.css('.text').text
+     harr << f.css('.text').css('a').text
+
+    # arr << a.css('span').css('em').text
    end
 
 
@@ -26,6 +37,8 @@ class StaticPagesController < ApplicationController
     @english = entries.css('.text')[0]
     @french = entries.css('.text')[1]
     @arr = arr
+    @frarr = frarr
+    @harr = harr
 
 
     # render template: 'static_pages/flash'
