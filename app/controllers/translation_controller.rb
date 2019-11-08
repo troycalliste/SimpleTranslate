5b772d5dd1ed25e1df4516791fc02c1e68be5ea7
troycalliste <troycallist@gmail.com>
2019-11-07 22:05:01 -0500

    flashcards button

--- app/controllers/translation_controller.rb
+++ app/controllers/translation_controller.rb
@@ -66,19 +66,19 @@ class TranslationController < ApplicationController
 
 
    def flashcards
-     project_id = ENV["CLOUD_PROJECT_ID"]
-
-     # Instantiates a client
-    translate = Google::Cloud::Translate.new project: project_id
-
-  # The text to translate
-    text = "Hello, world!"
-   # The target language
-    target = "ru"
-
-  # Translates some text into Russian
-    translation = translate.translate text, to: target
-    @txt = translation
+  #    project_id = ENV["CLOUD_PROJECT_ID"]
+  #
+  #    # Instantiates a client
+  #   translate = Google::Cloud::Translate.new project: project_id
+  #
+  # # The text to translate
+  #   text = "Hello, world!"
+  #  # The target language
+  #   target = "ru"
+  #
+  # # Translates some text into Russian
+  #   translation = translate.translate text, to: target
+  #   @txt = translation
    end
    def trnslate
 
