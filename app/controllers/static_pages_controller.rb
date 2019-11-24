951fcc9309d66b4c1b85492b2e1b2673f4b9a27d
troycalliste <troycallist@gmail.com>
2019-11-24 00:37:05 -0500

    google api

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -126,10 +126,10 @@ def translate
 
 
 
-   @info = params[:text]
-   outLang = params[:lang]
-  # @info = "Could be true"
-  # outLang = "French"
+   # @info = params[:text]
+   # outLang = params[:lang]
+  @info = "Could be true"
+  outLang = "French"
   project_id = 'simple-translate-1567281332603'
   # Instantiates a client
   translate = Google::Cloud::Translate.new project_id: project_id, keyfile: File.open('config/simpletranslate.json','r')
